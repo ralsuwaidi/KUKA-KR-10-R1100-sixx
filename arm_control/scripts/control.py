802c52276aa45b3314e61a3b353e69bdb3789033
Daniil Kirsanov <dankirsdot@gmail.com>
2019-11-12 01:31:35 +0300

    Fix the python script. From now on it will move all joints using a sine wave. You can specify the speed of movement, upper and lower limits.

--- arm_control/scripts/control.py
+++ arm_control/scripts/control.py
@@ -4,20 +4,46 @@ import rospy
 from std_msgs.msg import Float64
 import math
  
-def talker():
-    pub = rospy.Publisher('/kuka_arm/joint2_position_controller/command', Float64, queue_size=10)
+def move_joint(joint_name, speed, upper_limit, lower_limit):
+    pub = rospy.Publisher(joint_name, Float64, queue_size=10)
     rospy.init_node('talker', anonymous=True)
     rate_value = 50 # 50hz
     rate = rospy.Rate(rate_value)
-    while not rospy.is_shutdown():
+    if not rospy.is_shutdown():
         i = rospy.get_time()
-        position = math.sin(i/rate_value)*1.5 - 2
+        diff = (upper_limit - lower_limit)/2
+        offset = upper_limit - diff
+        position = math.sin(i/rate_value*speed)*diff + offset
         #rospy.loginfo(position)
         pub.publish(position)
         rate.sleep()
+
+def joint_name(number):
+    joint_name = '/kuka_arm/joint' + str(number) +'_position_controller/command'
+    return joint_name
  
 if __name__ == '__main__':
-    try:
-        talker()
-    except rospy.ROSInterruptException:
-        pass
+
+    speed = 100
+
+    while 1:
+        joint1 = joint_name(1)   
+        move_joint(joint1, speed, math.pi, -math.pi)
+        
+        joint2 = joint_name(2)
+        move_joint(joint2, speed, math.pi, -math.pi)
+
+        joint3 = joint_name(3)
+        move_joint(joint3, speed, math.pi, -math.pi)
+
+        joint4 = joint_name(4)
+        move_joint(joint4, speed, math.pi, -math.pi)
+
+        joint5 = joint_name(5)
+        move_joint(joint5, speed, math.pi, -math.pi)
+
+        joint6 = joint_name(6)
+        move_joint(joint6, speed, math.pi, -math.pi)
+
+        if rospy.ROSInterruptException:
+            pass

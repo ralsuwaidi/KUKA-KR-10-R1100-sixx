5eaaad5c57eeeb250dc35e9a474f5d78ac1927fe
Daniil Kirsanov <dankirsdot@gmail.com>
2019-11-12 19:01:18 +0300

    Updates the camera.py script. For now on it uses rospgk to obtain the path to the package and creates the folder to store images automatically

--- arm_sensor/scripts/camera.py
+++ arm_sensor/scripts/camera.py
@@ -1,6 +1,8 @@
 #! /usr/bin/python
 
+import os
 import rospy
+import rospkg
 # ROS Image message
 from sensor_msgs.msg import Image
 # ROS Image message -> OpenCV2 image converter
@@ -12,17 +14,14 @@ import cv2
 bridge = CvBridge()
 
 
-import os
-path = os.path.dirname(__file__)
-os.chdir(path)
 
 class ImageSaver:
 
-	def __init__(self):
+	def __init__(self, path):
 		self.ImageNumber = 0
+                self.ImagePath = path
 
 	def callback(self, msg):
-		#rospy.loginfo(msg.header)
 		try:
 			# Convert ROS Image message to OpenCV2
 			cv2_img = bridge.imgmsg_to_cv2(msg, "bgr8")
@@ -31,7 +30,7 @@ class ImageSaver:
 		else:
 			# Save OpenCV2 image as a jpeg
 			self.ImageNumber += 1
-			filename = './images/camera_image_' + str(self.ImageNumber) + '.jpg'			
+			filename = self.ImagePath + str(self.ImageNumber) + '.jpg'
 			cv2.imwrite(filename, cv2_img)
 
 	def main(self):
@@ -43,6 +42,31 @@ class ImageSaver:
 		# Spin until ctrl + c
 		rospy.spin()
 
-if __name__ == '__main__':
-	ImageSaver().main()
+
+
+if __name__ == '__main__':    
+    # get an instance of RosPack with the default search paths
+    rospack = rospkg.RosPack()
+    # get the file path for arm_senser package
+    pkg_path = rospack.get_path('arm_sensor')
+
+    # create the folder to store the images
+    folder_name = 'images'
+    folder_path = pkg_path + '/' + folder_name
+
+    try:
+        os.mkdir(folder_path)
+    except OSError:
+        print ("Creation of the directory %s failed" % folder_path)
+    else:
+        print ("Successfully created the directory %s " % folder_path)
+
+    # add an image name to the path
+    image_name = 'images_'
+    image_path = folder_path + '/' + image_name
+
+    # create image saver
+    saver = ImageSaver(image_path)
+    # run the saving process
+    saver.main()
 

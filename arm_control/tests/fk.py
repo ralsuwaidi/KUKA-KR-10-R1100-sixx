b899b59665ab8b269f91a5e45f5cf2cf26cddd19
Daniil Kirsanov <dankirsdot@gmail.com>
2019-11-23 14:32:18 +0300

    Combined arm_sensor and arm_control. Formatted all files. Completed integration test

--- arm_control/tests/fk.py
+++ arm_control/tests/fk.py
@@ -4,12 +4,15 @@ import numpy as np
 
 np.set_printoptions(precision=3)
 
+
 def DH_transform(theta, d, a, alpha):
     transform = np.array([[np.cos(theta), -np.sin(theta)*np.cos(alpha), np.sin(alpha)*np.sin(theta), a*np.cos(theta)],
-                          [np.sin(theta), np.cos(alpha)*np.cos(theta), -np.sin(alpha)*np.cos(theta), a*np.sin(theta)],
+                          [np.sin(theta), np.cos(alpha)*np.cos(theta), -
+                           np.sin(alpha)*np.cos(theta), a*np.sin(theta)],
                           [0, np.sin(alpha), np.cos(alpha), d],
                           [0, 0, 0, 1]])
     return transform
+
 
 def FK(theta, d, a, alpha):
 
--- arm_control/tests/integration_test.py
+++ arm_control/tests/integration_test.py
@@ -8,36 +8,39 @@ from std_msgs.msg import Float64
 from sensor_msgs.msg import JointState
 
 
-
 class controlTest(unittest.TestCase):
     publisher_working = False
     position = np.array([])
 
     def move_joint_to(self, pub, value):
-        #rospy.loginfo(value)
         pub.publish(value)
 
     def joint_name(self, number):
-        joint_name = '/kuka_arm/joint' + str(number) +'_position_controller/command'
+        joint_name = '/kuka_arm/joint' + \
+            str(number) + '_position_controller/command'
         return joint_name
 
     def callback(self, data):
         self.publisher_working = True
         self.position = data.position
 
-    def test_moveable(self):
+    def test_control(self):
 
         # Check it the joint state publisher is working
         rospy.init_node('integration')
         rospy.Subscriber('/kuka_arm/joint_states', JointState, self.callback)
-        
-        rospy.sleep(1) # wait some time
-        
-        self.assertTrue(self.publisher_working, "Joint state publisher is not working! Run the simulation!")
+
+        rospy.sleep(1)  # wait some time
+
+        self.assertTrue(self.publisher_working,
+                        "Joint state publisher is not working! Run the simulation!")
 
         # Save current robot position
         before_pose = np.array(self.position)
 
+        # Set rate for the publishers
+        rate_value = 50  # 50hz
+        rate = rospy.Rate(rate_value)
         # Create lists for joints and publishers
         joints = []
         pub = []
@@ -45,13 +48,9 @@ class controlTest(unittest.TestCase):
         for i in range(joints_number):
             joints.append(self.joint_name(i+1))
             pub.append(rospy.Publisher(joints[i], Float64, queue_size=10))
-        # Initialize node for publishers
-        #rospy.init_node('joints_talker', anonymous=True)
-        rate_value = 50 # 50hz
-        rate = rospy.Rate(rate_value)
 
         # Give a command for the robot to move to the initial state
-        desired_pose = np.array(np.deg2rad([0, 0, 0, 0, 0, 0])) # robot desired position
+        desired_pose = np.array(np.deg2rad([0, 0, 0, 0, 0, 0]))
         time = 200
         speed = 100
         for j in range(time):
@@ -61,9 +60,9 @@ class controlTest(unittest.TestCase):
                 except rospy.ROSInterruptException:
                     pass
             rate.sleep()
-        
+
         # Save robot position after the command
-        after_pose = np.array(self.position)       
+        after_pose = np.array(self.position)
 
         # Compare robot position before and after the command
         flag = np.array_equal(before_pose, after_pose)
@@ -72,9 +71,10 @@ class controlTest(unittest.TestCase):
         # Compare desired robot position and position after the command
         delta = 0.1
         for i in range(joints_number):
-            message = joints[i] + ' is not moved to the desired position!'
-            rospy.loginfo(np.abs(desired_pose[i] - after_pose[i]))
-            self.assertAlmostEqual(desired_pose[i], after_pose[i], msg="Robot did not move to the desired position!", delta = delta) 
+            msg = joints[i] + ' did not move to the desired position!'
+            self.assertAlmostEqual(
+                desired_pose[i], after_pose[i], msg=msg, delta=delta)
+
 
 if __name__ == '__main__':
     rostest.rosrun('arm_control', 'integration', controlTest, sysargs=None)
